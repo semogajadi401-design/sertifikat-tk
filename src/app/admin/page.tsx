ff8b1b6f761aba069520668f65d5152f506d1c19
semogajadi401-design <semogajadi401@gmail.com>
2026-05-31 19:15:38 +0800

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useState, useEffect, useRef } from 'react'
 import Navbar from '@/components/Navbar'
 import { supabase, Settings } from '@/lib/supabase'
